272fa855826f754cbb840d6c4e8219d24171c8d2
Rajan <rajanmasih154@gmail.com>
2025-02-05 01:49:05 +0530

    Added: Form of Login and Signup

---
+++ src/app/components/ui/signup/signup-form.tsx
@@ -0,0 +1,70 @@
+import { cn } from "../../../lib/utils";
+import { Button } from "../button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/src/app/components/ui/card";
+import { Input } from "../input";
+import { Label } from "@/src/app/components/ui/label";
+import Link from "next/link";
+
+export function SignupForm({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl">Signup</CardTitle>
+          <CardDescription>
+            Enter your email below to signup to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" type="text" placeholder="john doe" required />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              <Button type="submit" className="w-full">
+                Sign up
+              </Button>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              have an account?{" "}
+              <Link href="/login" className="underline underline-offset-4">
+                login
+              </Link>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
